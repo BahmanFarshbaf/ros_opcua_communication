b35aca72aff664350ebc62477d294d62d52d19b3
Bahman <bahman.farshbaf@gmail.com>
2025-10-16 12:46:58 +0200

    Update ros_server.py

--- ros_opcua_impl_python_opcua/scripts/ros_server.py
+++ ros_opcua_impl_python_opcua/scripts/ros_server.py
@@ -28,82 +28,76 @@ class ROSServer:
         rospy.init_node("opcua")
         self.server = Server()
         
-        with open('/data/workcell_smp_irb2600/config/irs_opcua_server.txt', 'r') as txt:
+        with open('/data/workcell_smp_irb22600/config/irs_opcua_server.txt', 'r') as txt:
             txtfile = txt.read()
             
         self.server.set_endpoint(txtfile)
-        # NEW: Load the XML file with the new string-based NodeIds
         self.server.import_xml("/data/workcell_smp_irb2600/config/irs_opcua_nodes_string_ids.xml")
         self.server.start()
         
-        # NEW: Get the namespace index dynamically. It's better than hardcoding 'ns=2'.
         self.idx = self.server.get_namespace_index(self.namespace_ros)
-        print(f"OPC UA Server using namespace index: {self.idx}")
+        # FIX: Replaced f-string with .format() for Python 2 compatibility
+        print("OPC UA Server using namespace index: {}".format(self.idx))
 
         handler = SubHandler()
         sub = self.server.create_subscription(100, handler)
-
-        # --- NEW: Browse for all variable nodes instead of looping a numeric range ---
         
-        # 1. Get the root node from which to start browsing
-        root_node = self.server.get_node(f"ns={self.idx};s=IRS_MES")
+        # --- Browse for all variable nodes ---
         
-        # 2. Recursively find all variable nodes under the root
+        # FIX: Replaced f-string
+        root_node_id = "ns={};s=IRS_MES".format(self.idx)
+        root_node = self.server.get_node(root_node_id)
+        
         all_variable_nodes = []
         self.browse_and_collect_vars(root_node, all_variable_nodes)
-        print(f"Found {len(all_variable_nodes)} total variable nodes.")
+        # FIX: Replaced f-string
+        print("Found {} total variable nodes.".format(len(all_variable_nodes)))
 
-        # 3. Define the nodes to exclude by their full string ID
+        # FIX: Replaced f-strings in the list definition
         nodes_to_exclude = [
-            f"ns={self.idx};s=IRS_MES.System.Client.SystemTimeUTC",
-            f"ns={self.idx};s=IRS_MES.System.Server.SystemTimeUTC"
+            "ns={};s=IRS_MES.System.Client.SystemTimeUTC".format(self.idx),
+            "ns={};s=IRS_MES.System.Server.SystemTimeUTC".format(self.idx)
         ]
 
-        # 4. Create the final list by filtering out the excluded nodes
         nodelist = [
             node for node in all_variable_nodes 
             if node.nodeid.to_string() not in nodes_to_exclude
         ]
         
-        # 5. Make all nodes in the final list writable
         for var in nodelist:
             var.set_writable()
 
-        print(f"Subscribing to data changes on {len(nodelist)} nodes after exclusions.")
+        # FIX: Replaced f-string
+        print("Subscribing to data changes on {} nodes after exclusions.".format(len(nodelist)))
         sub.subscribe_data_change(nodelist)
         
-        # --- End of new node discovery logic ---
-        
-        # NEW: Get the server time node and pass it directly to the timer function
-        server_time_node = self.server.get_node(f"ns={self.idx};s=IRS_MES.System.Server.SystemTimeUTC")
+        # FIX: Replaced f-string to get server time node
+        server_time_node_id = "ns={};s=IRS_MES.System.Server.SystemTimeUTC".format(self.idx)
+        server_time_node = self.server.get_node(server_time_node_id)
         rt = RepeatedTimer(30, timeupdater, server_time_node)
 
         print("ROS OPCUA Server initialized.")
         while not rospy.is_shutdown():
             rospy.spin()
              
-        time.sleep(0)  # used to be 60
+        time.sleep(0)
 
         self.server.stop()
         rt.stop()
         print("ROS OPCUA Server stopped.")
         quit()
 
-    # NEW: A recursive function to browse the node tree and collect all variables
     def browse_and_collect_vars(self, parent_node, collection_list):
         """
         Recursively browses from a parent node, finds all child nodes of type
         UAVariable, and adds them to the collection_list.
         """
         for child_node in parent_node.get_children():
-            # If the child is a variable, add it to our list
             if child_node.get_node_class() == ua.NodeClass.Variable:
                 collection_list.append(child_node)
-            # Recurse into the child to find its children, regardless of its type
             self.browse_and_collect_vars(child_node, collection_list)
 
 
-# NEW: The timeupdater function now accepts the node object directly
 def timeupdater(node_to_update):
     """Updates the timestamp on the given server time node."""
     node_to_update.set_value(ua.Variant(datetime.utcnow(), ua.VariantType.DateTime))
