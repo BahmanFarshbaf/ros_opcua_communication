0e312395ec8b36e3c2eed45770adaf95ee197472
Bahman <bahman.farshbaf@gmail.com>
2025-10-16 12:39:14 +0200

    new ros_server.py based on string nodes

--- ros_opcua_impl_python_opcua/scripts/ros_server.py
+++ ros_opcua_impl_python_opcua/scripts/ros_server.py
@@ -9,6 +9,7 @@ from opcua.ua import object_ids as o_ids
 from datetime import datetime
 from threading import Timer
 
+# This function remains unchanged
 def own_rosnode_cleanup():
     pinged, unpinged = rosnode.rosnode_ping_all()
     if unpinged:
@@ -16,6 +17,7 @@ def own_rosnode_cleanup():
         # noinspection PyTypeChecker
         rosnode.cleanup_master_blacklist(master, unpinged)
 
+# This class remains unchanged
 class SubHandler(object):
     def datachange_notification(self, node, val, data):
         print(node.get_display_name().Text, val)
@@ -25,40 +27,61 @@ class ROSServer:
         self.namespace_ros = rospy.get_param("/opcua/namespace")
         rospy.init_node("opcua")
         self.server = Server()
+        
         with open('/data/workcell_smp_irb2600/config/irs_opcua_server.txt', 'r') as txt:
             txtfile = txt.read()
+            
         self.server.set_endpoint(txtfile)
-        self.server.import_xml("/data/workcell_smp_irb2600/config/irs_opcua_nodes.xml")
+        # NEW: Load the XML file with the new string-based NodeIds
+        self.server.import_xml("/data/workcell_smp_irb2600/config/irs_opcua_nodes_string_ids.xml")
         self.server.start()
+        
+        # NEW: Get the namespace index dynamically. It's better than hardcoding 'ns=2'.
+        self.idx = self.server.get_namespace_index(self.namespace_ros)
+        print(f"OPC UA Server using namespace index: {self.idx}")
 
         handler = SubHandler()
         sub = self.server.create_subscription(100, handler)
 
-        nodelist = []
-        try:
-            for nd in range(6001, 6127):
-                    var = self.server.get_node('ns=2;i=' + str(nd))
-                    nodelist.append(var)
-                    dtype = var.get_data_type()
-                    if dtype.NamespaceIndex == 0 and dtype.Identifier in o_ids.ObjectIdNames:
-                        dtype_name = o_ids.ObjectIdNames[dtype.Identifier]
-                        set_node_type(dtype_name, var)
-                    else:
-                        dtype_name = dtype.to_string()
-                        rank = var.get_value_rank()
-                    self.server.get_node('ns=2;i=' + str(nd)).set_writable()
-        except:
-            print('Subscribed on ' + str(nd - 6001) + ' Nodes!')
-        del nodelist[3] #client time
-        del nodelist[6] #server time
+        # --- NEW: Browse for all variable nodes instead of looping a numeric range ---
+        
+        # 1. Get the root node from which to start browsing
+        root_node = self.server.get_node(f"ns={self.idx};s=IRS_MES")
+        
+        # 2. Recursively find all variable nodes under the root
+        all_variable_nodes = []
+        self.browse_and_collect_vars(root_node, all_variable_nodes)
+        print(f"Found {len(all_variable_nodes)} total variable nodes.")
 
-        rt = RepeatedTimer(30, timeupdater, self.server)
+        # 3. Define the nodes to exclude by their full string ID
+        nodes_to_exclude = [
+            f"ns={self.idx};s=IRS_MES.System.Client.SystemTimeUTC",
+            f"ns={self.idx};s=IRS_MES.System.Server.SystemTimeUTC"
+        ]
+
+        # 4. Create the final list by filtering out the excluded nodes
+        nodelist = [
+            node for node in all_variable_nodes 
+            if node.nodeid.to_string() not in nodes_to_exclude
+        ]
+        
+        # 5. Make all nodes in the final list writable
+        for var in nodelist:
+            var.set_writable()
+
+        print(f"Subscribing to data changes on {len(nodelist)} nodes after exclusions.")
         sub.subscribe_data_change(nodelist)
+        
+        # --- End of new node discovery logic ---
+        
+        # NEW: Get the server time node and pass it directly to the timer function
+        server_time_node = self.server.get_node(f"ns={self.idx};s=IRS_MES.System.Server.SystemTimeUTC")
+        rt = RepeatedTimer(30, timeupdater, server_time_node)
 
         print("ROS OPCUA Server initialized.")
         while not rospy.is_shutdown():
             rospy.spin()
-            
+             
         time.sleep(0)  # used to be 60
 
         self.server.stop()
@@ -66,9 +89,26 @@ class ROSServer:
         print("ROS OPCUA Server stopped.")
         quit()
 
-def timeupdater(serverself):
-    serverself.get_node('ns=2;i=6008').set_value(ua.Variant(datetime.utcnow(), ua.VariantType.DateTime))
+    # NEW: A recursive function to browse the node tree and collect all variables
+    def browse_and_collect_vars(self, parent_node, collection_list):
+        """
+        Recursively browses from a parent node, finds all child nodes of type
+        UAVariable, and adds them to the collection_list.
+        """
+        for child_node in parent_node.get_children():
+            # If the child is a variable, add it to our list
+            if child_node.get_node_class() == ua.NodeClass.Variable:
+                collection_list.append(child_node)
+            # Recurse into the child to find its children, regardless of its type
+            self.browse_and_collect_vars(child_node, collection_list)
 
+
+# NEW: The timeupdater function now accepts the node object directly
+def timeupdater(node_to_update):
+    """Updates the timestamp on the given server time node."""
+    node_to_update.set_value(ua.Variant(datetime.utcnow(), ua.VariantType.DateTime))
+
+# This function remains unchanged
 def set_node_type(dtype_name, var):
     if dtype_name == 'DateTime':
         dv = ua.Variant(datetime.utcfromtimestamp(0.0), ua.VariantType.DateTime)
@@ -76,6 +116,7 @@ def set_node_type(dtype_name, var):
         return None
     return var.set_value(dv)
 
+# This class remains unchanged
 class RepeatedTimer(object):
     def __init__(self, interval, function, *args, **kwargs):
         self._timer     = None
